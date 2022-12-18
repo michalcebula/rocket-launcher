43bc1baba980a7a3a5029a8ea00d320c6dc23d83
michalcebula <michalcebula92@gmail.com>
2022-12-18 19:00:23 +0100

    refactor: use LaunchHandler service in launches controller

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -4,22 +4,13 @@ require 'net/http'
 class LaunchesController < ApplicationController
   # See the https://docs.spacexdata.com/#intro for more information about SpaceX API
   def create
-    launch_attributes = LaunchDTO.new(launch_params(params))
-
     response = SpaceXData::V3::ListRockets.new.call
-    rockets_handler = RocketsHandler.new(response.body)
-    rocket = rockets_handler.find_rocket(launch_attributes.rocket_id)
+    result = LaunchHandler.new(launch_params(params), response.body).call
 
-    unless rocket
-      return render  json: { error: 'Invalid rocket_id' }, status: :unprocessable_entity; end
-    unless launch_attributes.launch_time
-      return render json: { error: 'launch_time must be present' }, status: :unprocessable_entity; end
-    unless launch_attributes.periapsis_distance_valid?
-      return render json: { error: 'periapsis_km is too small' }, status: :unprocessable_entity; end
+    error_message = result[:error]
+    return render json: { error: error_message }, status: :unprocessable_entity if error_message
 
-    cost_per_launch = rockets_handler.cost_per_launch(rocket)
-    launch_attributes = launch_attributes.full.merge!(cost_per_launch: cost_per_launch)
-    
+    launch_attributes = result[:attributes]
     return render json: launch_attributes, status: :created if Launch.create(launch_attributes)
 
     render json: { error: 'Something went wrong. Please, try again'}, status: :unprocessable_entity
