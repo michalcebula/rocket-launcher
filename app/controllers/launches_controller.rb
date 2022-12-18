3851df023c7d732f0dedecf5111867702498708f
michalcebula <michalcebula92@gmail.com>
2022-12-18 14:24:33 +0100

    refactor: use LaunchDTO in launch controller

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -4,23 +4,23 @@ require 'net/http'
 class LaunchesController < ApplicationController
   # See the https://docs.spacexdata.com/#intro for more information about SpaceX API
   def create
-    launch_attributes = launch_params(params)
-    launch_time = launch_attributes.fetch(:launch_time)
-    periapsis_km = launch_attributes.fetch(:periapsis_km)
+    launch_attributes = LaunchDTO.new(launch_params(params))
 
     response = SpaceXData::V3::ListRockets.new.call
     rockets_handler = RocketsHandler.new(response.body)
+    rocket = rockets_handler.find_rocket(launch_attributes.rocket_id)
 
-    rocket = rockets_handler.find_rocket(launch_attributes.fetch(:rocket_id))
-
-    return render json: { error: 'Invalid rocket_id' }, status: :unprocessable_entity unless rocket
-    return render json: { error: 'launch_time must be present' }, status: :unprocessable_entity if launch_time.blank?
-    return render json: { error: 'periapsis_km is too small' }, status: :unprocessable_entity if periapsis_km < 150
+    unless rocket
+      return render  json: { error: 'Invalid rocket_id' }, status: :unprocessable_entity; end
+    unless launch_attributes.launch_time
+      return render json: { error: 'launch_time must be present' }, status: :unprocessable_entity; end
+    unless launch_attributes.periapsis_distance_valid?
+      return render json: { error: 'periapsis_km is too small' }, status: :unprocessable_entity; end
 
     cost_per_launch = rockets_handler.cost_per_launch(rocket)
-    launch_attributes.merge!(cost_per_launch: cost_per_launch)
+    launch_detials = launch_attributes.full.merge!(cost_per_launch: cost_per_launch)
 
-    render json: launch_attributes, status: :created
+    render json: launch_detials, status: :created
   end
 
   private
